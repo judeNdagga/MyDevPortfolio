f9aec591037dd4b868e1370ed8a556639244a121
judeNdagga <ndaggajude88@gmail.com>
2023-09-05 11:30:10 +0300

    changed name

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -8,7 +8,7 @@ export default function Home(): JSX.Element {
     <Layout>
       <Box color="white" fontFamily="Roboto" pos="relative" zIndex={1}>
         <Head>
-          <title>v1b3m | Home</title>
+          <title>Jude | Home</title>
           <meta
             name="description"
             content="I am a talented Software engineer proficient in all phases of the Software development lifecycle"
